538dcff8fb0af347393e35dd3601e9f73b4cf51e
Muhammad-Abid11 <abidkhan11845@gmail.com>
2023-01-29 02:37:53 +0500

    Use "useState"

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // in class components state used as
+  // constructor(){
+  //  this.state={
+  //    name:"abid"
+  //  }
+  // }
+
+  //  for update in class component we use " setState({
+  //  name:"Update"
+  // }) "   
+
+
+  const [name, setName] = useState("Muhammad Abid from useState");
+  //like destructuring
+  //[property,function]=useState("Default Value")
+  const [email, setEmail] = useState("abidkhan@gmail.com");
+  const [arr, setArr] = useState("car", "lion", "leopard");
+
+  const handle = () => {
+    setName("Abid")
+    setEmail("Abid@gmail.com")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <p>Welcome{'\n'}{'\n'}
+      </p>
+      <p>React Hooks    {'\n'}</p>
+      <p>Hello World ---> {name} Expo!</p>
+      <p>email :   {email} Expo!</p>
+      <p>Class 31</p>
+      <button onClick={() => setName("Sajid")}>Update Name</button>
+      <br />
+      <button onClick={() => {
+        setName("Taha")
+        setEmail("Taha@gmail.com")
+      }
+      }>Update Name and Email</button>
+      <br />
+      <button onClick={handle}>set by function</button>
+    </div >
   );
 }
 

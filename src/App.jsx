6846dd79ea71c9e007bd1a7a527b2cc4bef86476
Muhammad-Abid11 <abidkhan11845@gmail.com>
2023-01-29 02:50:00 +0500

    "useState" used  in array

--- src/App.jsx
+++ src/App.jsx
@@ -19,17 +19,22 @@ function App() {
   //like destructuring
   //[property,function]=useState("Default Value")
   const [email, setEmail] = useState("abidkhan@gmail.com");
-  const [arr, setArr] = useState("car", "lion", "leopard");
+  // we can also used in array and object 
+  const [arr, setArr] = useState(["car", "lion", "leopard"]);
 
   const handle = () => {
     setName("Abid")
     setEmail("Abid@gmail.com")
   }
+
+  const arrUpdate = () => {
+    setArr([...arr, "Dog"])
+
+  }
   return (
     <div className="App">
-      <p>Welcome{'\n'}{'\n'}
-      </p>
-      <p>React Hooks    {'\n'}</p>
+      <p>Welcome</p>
+      <p>React Hooks</p>
       <p>Hello World ---> {name} Expo!</p>
       <p>email :   {email} Expo!</p>
       <p>Class 31</p>
@@ -42,6 +47,13 @@ function App() {
       }>Update Name and Email</button>
       <br />
       <button onClick={handle}>set by function</button>
+
+      <ul>
+        {arr.map((v, i) => {
+          return <li key={i}>{v}</li>
+        })}
+      </ul>
+      <button onClick={arrUpdate}>Array Update</button>
     </div >
   );
 }
